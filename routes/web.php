3bf105036b2721e8bc7799c33a2d3646fde6038f
hanifann <hanifannurulhaq@outlook.com>
2022-06-21 23:52:43 +0700

    edit

--- routes/web.php
+++ routes/web.php
@@ -20,33 +20,36 @@ use App\Http\Controllers\TravelController;
 |
 */
 
-Route::get('/',[AuthController::class, 'index'])->name('index');
+Route::group(['schema' => 'https'], function () {
+ Route::get('/',[AuthController::class, 'index'])->name('index');
 
-Route::post('/',[AuthController::class, 'login'])->name('user.login');
-Route::get('/logout',[AuthController::class, 'logout'])->name('user.logout');
+    Route::post('/',[AuthController::class, 'login'])->name('user.login');
+    Route::get('/logout',[AuthController::class, 'logout'])->name('user.logout');
 
-Route::get('/register',[RegisterController::class, 'index']);
+    Route::get('/register',[RegisterController::class, 'index']);
 
-Route::post('/register',[RegisterController::class, 'register'])->name('user.register');
+    Route::post('/register',[RegisterController::class, 'register'])->name('user.register');
 
-Route::get('/403', [ErrorController::class, 'forbidden'])->name('error.403');
+    Route::get('/403', [ErrorController::class, 'forbidden'])->name('error.403');
 
-Route::group(['middleware' => ['token']], function() {
-    Route::get('/banner', [BannerController::class, 'index']);
-    Route::post('/banner', [BannerController::class, 'postBanner'])->name('postBanner');
-    Route::put('/banner', [BannerController::class, 'updateBanner'])->name('updateBanner');
-    Route::delete('/banner', [BannerController::class, 'deleteBanner'])->name('deleteBanner');
+    Route::group(['middleware' => ['token']], function() {
+        Route::get('/banner', [BannerController::class, 'index']);
+        Route::post('/banner', [BannerController::class, 'postBanner'])->name('postBanner');
+        Route::put('/banner', [BannerController::class, 'updateBanner'])->name('updateBanner');
+        Route::delete('/banner', [BannerController::class, 'deleteBanner'])->name('deleteBanner');
 
-    Route::get('/artikel', [RecomendationController::class, 'index']);
-    Route::post('/artikel', [RecomendationController::class, 'postRecomendation'])->name('postArtikel');
-    Route::put('/artikel', [RecomendationController::class, 'updateRecomendation'])->name('updateArtikel');
-    Route::delete('/artikel', [RecomendationController::class, 'deleteRecomendation'])->name('deleteArtikel');
+        Route::get('/artikel', [RecomendationController::class, 'index']);
+        Route::post('/artikel', [RecomendationController::class, 'postRecomendation'])->name('postArtikel');
+        Route::put('/artikel', [RecomendationController::class, 'updateRecomendation'])->name('updateArtikel');
+        Route::delete('/artikel', [RecomendationController::class, 'deleteRecomendation'])->name('deleteArtikel');
 
-    Route::get('/travelling', [TravelController::class, 'index']);
-    Route::post('/travelling', [TravelController::class, 'postTravel'])->name('postTravel');
-    Route::put('/travelling', [TravelController::class, 'updateTravel'])->name('updateTravel');
-    Route::delete('/travelling', [TravelController::class, 'deleteTravel'])->name('deleteTravel');
+        Route::get('/travelling', [TravelController::class, 'index']);
+        Route::post('/travelling', [TravelController::class, 'postTravel'])->name('postTravel');
+        Route::put('/travelling', [TravelController::class, 'updateTravel'])->name('updateTravel');
+        Route::delete('/travelling', [TravelController::class, 'deleteTravel'])->name('deleteTravel');
 
+    });
+    
 });
 
 
